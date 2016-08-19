ebb3452a40484d650f12955f89e8ee6379839307
Nicolas <Nicolas@DESKTOP-LGJ4647>
2016-08-18 21:07:58 -0300

    Project creation

---
+++ src/com/jouwee/commons/simulation/RealTimeSimulationPanel.java
@@ -0,0 +1,9 @@
+package com.jouwee.commons.simulation;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class RealTimeSimulationPanel {
+    
+}
---
+++ src/com/jouwee/commons/simulation/RealTimeSimulationThread.java
@@ -0,0 +1,12 @@
+package com.jouwee.commons.simulation;
+
+/**
+ * Real-time simulation thread
+ * 
+ * @author Jouwee
+ */
+public class RealTimeSimulationThread extends Thread {
+    
+    
+    
+}
