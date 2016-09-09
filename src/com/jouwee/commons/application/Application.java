bba53606b79336730203ae0579d6d7b7d57c7313
Nicolas <Nicolas@DESKTOP-LGJ4647>
2016-09-09 20:48:46 -0300

    Implemented the process ThresholdProcess, for applying a threshold binarization in every pixel of the image. Implemented necessary methods in PixelProcess

--- src/com/jouwee/commons/application/Application.java
+++ src/com/jouwee/commons/application/Application.java
@@ -1,8 +1,10 @@
 package com.jouwee.commons.application;
 
+import com.jouwee.commons.mvc.Model;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -10,25 +12,82 @@ import javafx.stage.Stage;
  * Application
  * 
  * @author Nícolas Pohren
+ * @param <T> Model type of the application
  */
-public class Application extends javafx.application.Application {
+public class Application<T extends Model> extends javafx.application.Application {
 
+    /** Model */
+    private T model;
+    /** Body */
+    private JavaFXView body;
+    /** Main panel */
+    private BorderPane mainPanel;
+
+    /**
+     * Creates a new application
+     */
+    public Application(T model) {
+        super();
+        this.model = model;
+//        body = new ...;
+    }
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Hello World!");
         StackPane root = new StackPane();
-        root.getChildren().add(buildMainPanel());
+        root.getChildren().add(getMainPanel());
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
-    
+
     /**
-     * Builds the main panel
+     * Returns the main panel
      * 
-     * @return Node
+     * @return BorderPane
      */
-    private Node buildMainPanel() {
-        return new Button("dqw");
+    public BorderPane getMainPanel() {
+        if (mainPanel == null) {
+            mainPanel = new BorderPane();
+        }
+        return mainPanel;
     }
-    
+
+    /**
+     * Returns the body
+     * 
+     * @return JavaFXView
+     */
+    public JavaFXView getBody() {
+        return body;
+    }
+
+    /**
+     * Sets the body
+     * 
+     * @param body 
+     */
+    public void setBody(JavaFXView body) {
+        this.body = body;
+        getMainPanel().setCenter(body);
+    }
+
+    /**
+     * Returns the model
+     * 
+     * @return T
+     */
+    public T getModel() {
+        return model;
+    }
+
+    /**
+     * Sets the model
+     * 
+     * @param model 
+     */
+    public void setModel(T model) {
+        this.model = model;
+    }
+
 }
---
+++ src/com/jouwee/commons/application/JavaFXView.java
@@ -0,0 +1,46 @@
+
+package com.jouwee.commons.application;
+
+import com.jouwee.commons.mvc.Model;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * JavaFX view
+ * 
+ * @author Nícolas Pohren
+ * @param <M> View model type
+ */
+public abstract class JavaFXView<M extends Model> extends BorderPane {
+
+    /** Model */
+    private M model;
+    
+    /**
+     * Cria uma nova view
+     * 
+     * @param model
+     */
+    public JavaFXView(M model) {
+        super();
+        this.model = model;
+    }
+
+    /**
+     * Returns the model
+     * 
+     * @return M
+     */
+    public M getModel() {
+        return model;
+    }
+
+    /**
+     * Sets the model
+     * 
+     * @param model 
+     */
+    public void setModel(M model) {
+        this.model = model;
+    }
+    
+}
